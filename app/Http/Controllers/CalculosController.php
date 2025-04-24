b493a7e6b29712cf96ca4efd9bb07fd02fb29494
LiviaRGS <ticorin100@gmail.com>
2025-04-24 10:16:38 -0300

    Outras Contas

--- app/Http/Controllers/CalculosController.php
+++ app/Http/Controllers/CalculosController.php
@@ -9,4 +9,13 @@ class CalculosController extends Controller
     function soma($x, $y){
         return 'Soma: '.$x+$y;
     }
+    function sub($x, $y){
+        return 'Subtração: '.$x-$y;
+    }
+    function mult($x, $y){
+        return 'Multiplicação: '.$x*$y;
+    }
+    function divi($x, $y){
+        return 'Divisão: '.$x/$y;
+    }
 }
